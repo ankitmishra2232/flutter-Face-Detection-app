35b64ce66e501eb3b3dfa3932fb8131365fe8274
Ankit Mishra <ankitmishra.in.com@gmail.com>
2023-01-04 00:38:05 +0530

    utils file

--- ml_face_detection/lib/home.dart
+++ ml_face_detection/lib/home.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,6 +11,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  bool isWorking = false;
+  CameraController? cameraController;
+  FaceDetector? faceDetector;
+  Size? size;
+  List<Face>? faceslist;
+  CameraDescription? description;
+  CameraLensDirection cameraDirection = CameraLensDirection.front;
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
